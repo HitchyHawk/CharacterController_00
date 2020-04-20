f70da6bd0ee273b216cc65efcb614d4aa83a3344
Hitch_Hawk <47764116+HitchyHawk@users.noreply.github.com>
2020-04-20 14:44:15 -0700

    normal detection and key input

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    Color rayColor = new Color(1, 0, 0);
+    Vector3 pos;
+    public Vector3 endLocal = new Vector3(0,1,0);
+    Vector3 cont = new Vector3(0,1.5f,0);
+    Vector3 norm = new Vector3(0,1,0);
+    Vector3 vel = new Vector3(0, -1, 0);
+
+    public float nDotg = 1;
+
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        pos = transform.position;
+        nDotg = Vector3.Dot(vel, norm);
+        Debug.DrawLine(pos, cont, rayColor);
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            //this will give position of collision
+            cont = contact.point;
+            //this gives normal of surface
+            norm = contact.normal;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    int[] HoVeInput = {0,0};
+    
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if      (Input.GetKey(KeyCode.D))   HoVeInput[0] = 1;
+        else if (Input.GetKey(KeyCode.A))   HoVeInput[0] = -1;
+        else                                HoVeInput[0] = 0;
+
+        if      (Input.GetKey(KeyCode.W))   HoVeInput[1] = 1;
+        else if (Input.GetKey(KeyCode.S))   HoVeInput[1] = -1;
+        else                                HoVeInput[1] = 0;
+
+        //tells us what keys are pressed, in a form that is easy to math with.
+        Debug.Log(HoVeInput[0] + ", " + HoVeInput[1]);
+
+    }
+}
