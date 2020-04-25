9e44ec269dc331925f380226a36d9dab34cc938b
Hitch_Hawk <47764116+HitchyHawk@users.noreply.github.com>
2020-04-24 17:20:33 -0700

    Collisions are now fine. No crashing, still needs a bit of polish but overall I think its good. Working on camera controls now.

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -8,28 +8,30 @@ public class PlayerCollision : MonoBehaviour
     PlayerController controls;
     Collider coll;
     public LayerMask collisionMask = 0;
-    public float tollerace = 0.01f;
-    public float speed = 1;
-    public float jumpSpeed = 2;
-    public float drag = 0.2f;
-    public float grav = 1;
-    public float collisionRes = 5;
-    public float rayOffset = 0.5f;
-    public float rayHeight = 0.75f;
+    public int checksPerFrame = 5;
+    private float baseSpeed,sprintSpeed,jumpSpeed,speed,grav,drag;
+   
+    
+    float rayOffset = 0.5f;
+    float rayHeight = 0.75f;
 
     public bool isGrounded = false;
+    public bool jumped = false;
 
     public Vector3 velocity = new Vector3(0,0,0);
     private Vector3 respawn;
     private Vector3 position;
-
-    private float temp = 0;
 
     void Start()
     {
         respawn = transform.position;
         controls = GetComponent<PlayerController>();
         coll = GetComponent<Collider>();
+        baseSpeed = speed = controls.baseSpeed;
+        sprintSpeed = controls.sprintSpeed;
+        jumpSpeed = controls.jumpSpeed;
+        drag = controls.drag;
+        grav = controls.grav;
     }
 
     void FixedUpdate()
@@ -37,33 +39,36 @@ public class PlayerCollision : MonoBehaviour
         //respawn position
         if (controls.reset) {
             transform.position = respawn;
-            velocity = Vector3.zero;
+            velocity = Vector3.zero+Vector3.up*0.01f;
             isGrounded = false;
         }
+        if (controls.isSprint){
+            speed = baseSpeed * sprintSpeed;
+        }
+        else speed = baseSpeed;
+        jumped = false;
 
         //sets our origin for calulating movement
         position = transform.position;
-        velocity += (Vector3.down*grav + Vector3.Normalize(controls.HoVeInput) * speed - new Vector3(velocity.x, 0, velocity.z) * drag) * Time.deltaTime;
-
+        velocity += (controls.HoVeInput * speed - new Vector3(velocity.x, 0, velocity.z) * drag) * Time.deltaTime;
+        
+        if (isGrounded)
+        {
+            if (controls.isJump)
+            {
+                isGrounded = false;
+                jumped = true;
+                velocity += Vector3.up * jumpSpeed * Time.deltaTime;
+            }
+        }
         //calculate horizontal collision and slopes slopes due to velocity
         velocity = CollisionVelocityAdjustment(velocity);
+
         //This is where we will end up
         position += velocity;
 
-        //are we grounded?
-        //else apply appropriate gravity
-        if (isGrounded){
-            if (controls.isJump){
-                isGrounded = false;
-                position += Vector3.up * jumpSpeed*Time.deltaTime;
-                velocity += Vector3.up * jumpSpeed *Time.deltaTime;
-            }
-
-        }
-       
-        Debug.DrawRay(transform.position, velocity * 10, new Color(1, 0, 0));
+        //Debug.DrawRay(transform.position, velocity * 10, new Color(1, 0, 0));
         transform.position = position;
-        Debug.Log("END OF CYCLE---------------------------");
     }
     Vector3 CollisionVelocityAdjustment(Vector3 v)
     {
@@ -71,30 +76,30 @@ public class PlayerCollision : MonoBehaviour
         Vector3 p1 = position + Vector3.up * rayHeight / 2;
         Vector3 p2 = position - Vector3.up * rayHeight / 2;
 
-        Debug.DrawRay(p2, Vector3.up, new Color(0, 1, 1));
         //downwards cast to folow the slope down or up
-        Debug.Log("Collide with floor, for proper terrain Movement?");
-        if (Physics.SphereCast(p2, rayOffset, Vector3.down, out hit, Mathf.Abs(v.y))) {
-            p1 += Vector3.Normalize(v) * hit.distance;
-            p2 += Vector3.Normalize(v) * hit.distance;
-            //move collision sphere there to calculate how to deal with floor or ramp
-            v -= Vector3.Dot(hit.normal, v) * hit.normal;
-            Debug.Log("Normal floor hit:" + hit.normal);
-            Debug.DrawRay(p2, Vector3.up, new Color(0, 1, 0));
-
+        if (Physics.SphereCast(p2, rayOffset, Vector3.down, out hit, Mathf.Abs(v.y) + grav * Time.deltaTime) && !jumped) {
+            int i = 0;
+            do {
+                //move collision sphere there to calculate how to deal with floor or ramp
+                v -= Vector3.Dot(hit.normal, v) * hit.normal;
+                i++;
+            } while (Physics.SphereCast(p2, rayOffset, Vector3.down, out hit, Mathf.Abs(v.y) + grav * Time.deltaTime) && i < checksPerFrame);
             isGrounded = true;
         } else {
-            Debug.Log("No ground below to map movement to properly");
+            v += Vector3.down * grav * Time.deltaTime;
             isGrounded = false;
         }
 
-        Debug.Log("Checking this horizontal slope wall check");
-        //horizontal cast to follow the slope if about to crash into it
-        if (Physics.CapsuleCast(p1, p2, rayOffset, v, out hit, Vector3.Magnitude(v))) {
-            v -= Vector3.Dot(hit.normal, v) * hit.normal;
-            Debug.Log("Yes slope of wall/ramp, normal:" + hit.normal);
-        }
+        //walking on slopes or walls
+        if (Physics.CapsuleCast(p1, p2, rayOffset, v, out hit, Vector3.Magnitude(v))){
+            int i = 0;
+            do { 
+                v -= Vector3.Dot(hit.normal, v) * hit.normal;
+                i++;
+            } while (Physics.CapsuleCast(p1, p2, rayOffset, v, out hit, Vector3.Magnitude(v)) && i < checksPerFrame) ;
 
+            if (i == checksPerFrame) v *= 0;
+        }
         return v;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Vector3 HoVeInput = new Vector3(0,0,0);
+    private Vector3 tempHV;
+    public float grav = 0.75f;
+    public float drag = 20;
+
+    public float baseSpeed = 1;
+    public float sprintSpeed = 1.5f;
+    public float jumpSpeed = 2;
+
     public bool isJump = false;
     public bool reset = false;
+    public bool isSprint = false;
 
-    
+    public GameObject cameraObject;
+    public GameObject cameraPivot;
+    public bool mouseLocked = true;
+    public float sensitivity = 1;
+    public float baseRadius = 6;
+    public float baseHeight = 1;
+    public float theta = 0;
+    float radius = 0.1f;
+    Vector3 cameraPosition = Vector3.zero;
+
+    private Vector3 center;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        /*
+        
         if      (Input.GetKey(KeyCode.D))   HoVeInput[0] = 1;
         else if (Input.GetKey(KeyCode.A))   HoVeInput[0] = -1;
         else                                HoVeInput[0] = 0;
@@ -22,15 +48,41 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKey(KeyCode.S))   HoVeInput[2] = -1;
         else                                HoVeInput[2] = 0;
 
+        if (Input.GetKey(KeyCode.LeftShift)) isSprint = true;
+        else                                 isSprint = false;
+
         if (Input.GetKey(KeyCode.Space))    isJump = true;
         else                                isJump = false;
-        */
+        
         if (Input.GetKey(KeyCode.R))        reset = true;
-        //else                                reset = false;
+        else                                reset = false;
 
+        if (Input.GetKey(KeyCode.Escape)) mouseLocked = false;
 
-        //tells us what keys are pressed, in a form that is easy to math with.
-        //Debug.Log(HoVeInput[0] + ", " + HoVeInput[2]);
+        if (mouseLocked) {
+            //Debug.Log("H: " + Input.GetAxis("Mouse X") + "\tV: " + Input.GetAxis("Mouse Y"));
+            theta += Input.GetAxis("Mouse X") * sensitivity*Time.deltaTime;
+            if (Mathf.Abs(theta) > 2 * Mathf.PI) theta -= 2 * Mathf.PI * theta / Mathf.Abs(theta);
+            cameraPivot.transform.localEulerAngles = new Vector3(0, Mathf.Rad2Deg*theta, 0);
+
+            cameraObject.transform.localPosition = new Vector3
+                (0,
+                ( baseHeight - cameraObject.transform.localPosition.y) / 10,
+                (-baseRadius - cameraObject.transform.localPosition.z) / 10) * Time.deltaTime;
+            
+        }
+        else {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        tempHV = Vector3.Normalize(HoVeInput);
+
+        
+        HoVeInput[0] =  tempHV[0]*Mathf.Cos(-theta) - tempHV[2]* Mathf.Sin(-theta);
+        HoVeInput[2] =  tempHV[0]*Mathf.Sin(-theta) + tempHV[2]* Mathf.Cos(-theta);
+        Debug.Log("cos: "+ Mathf.Cos(theta) + "\tsin: " + Mathf.Sin(theta));
+        Debug.Log(Vector3.Magnitude(HoVeInput));
+
 
     }
+
 }
