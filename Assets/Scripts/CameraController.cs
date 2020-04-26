adb0873963d5dc9b1dcaa7aec997b6ebd4ca558b
Hitch_Hawk <47764116+HitchyHawk@users.noreply.github.com>
2020-04-25 21:19:27 -0700

    Something weird happening with phi, the line I commented out is crucial to making it work, but it shouldn't it was there by accident. Need to do some paper work to figure out whats happening, Going over my program step by step. Also study sphere coordinates.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,24 +6,22 @@ public class CameraController : MonoBehaviour
 {
     public GameObject cameraObject;
     PlayerController controls;
+    public Vector3 cameraDirection = new Vector3(0, 0, -1);
+    public float theta = 0; //y rot
+    public float phi   = 0; //x rot
+    [Range(0,Mathf.PI*0.5f - 0.3f)]
+    public float phiCutOff = Mathf.PI * 0.5f - 0.3f;
 
-    public float radius;
-    public float rads;
+    private Vector3 direction;
+    private Vector3 position ;
 
-    public float height = 0;
-
-
-    public Vector3 originPos = new Vector3(0,0,-1);
-    Vector3 position;
-    //for calculating collisions
-    Vector3 direction;
+    public float radius = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         controls = GetComponent<PlayerController>();
-        radius = Vector3.Magnitude(cameraObject.transform.position);
-        position = originPos;
+        cameraObject.transform.localPosition = position = controls.cameraOffset;
     }
 
     // Update is called once per frame
@@ -32,44 +30,30 @@ public class CameraController : MonoBehaviour
     /// </summary>
     void FixedUpdate()
     {
-        rads = controls.theta;
-        height = -controls.h;
-
-
-        radius = (controls.baseRadius + radius * (controls.cameraSmooth - 1)) / controls.cameraSmooth;
-
-        
-
-        position[0] = originPos[0] * Mathf.Cos(-rads) - originPos[2] * Mathf.Sin(-rads);
-        position[1] = height+controls.baseHeight;
-        position[2] = originPos[0] * Mathf.Sin(-rads) + originPos[2] * Mathf.Cos(-rads);
-        
-
-        //originPos = new Vector3(originPos[0] * Mathf.Cos(-rads) - originPos[2] * Mathf.Sin(-rads), 0, originPos[0] * Mathf.Sin(-rads) + originPos[2] * Mathf.Cos(-rads));
-        
-
-        radius = cameraCollisionR(position, radius);
-        position.x *= radius;
-        position.z *= radius;
-
-        cameraObject.transform.localEulerAngles = new Vector3(-height / (controls.heightLimit+controls.baseHeight) * controls.cameraXRot, Mathf.Rad2Deg * rads, 0);
-        cameraObject.transform.localPosition = position;
-    }
-
-    float cameraCollisionR(Vector3 pos, float r)
-    {
-
-        Vector3 origin = controls.transform.position;
-
-        RaycastHit hit;
-
-        if (Physics.SphereCast(origin, controls.cameraSize, new Vector3(pos.x * r, 0, pos.z * r), out hit))
-        {
-            Debug.DrawRay(origin, pos * r, new Color(0, 1, 1));
-            r = hit.distance - controls.cameraSize;
-            if (r > controls.baseRadius) r = controls.baseRadius;
+        theta += Mathf.Atan(controls.camHSpeed / controls.cameraBgR  );
+        if (Mathf.Abs(theta) > 2 * Mathf.PI) theta -= 2 * Mathf.PI * theta / Mathf.Abs(theta);
+        //so the camera doesnt rotate upside down and slows down when it approaches the top;
+        if (phi * controls.camVSpeed < 0 ){
+            phi += -Mathf.Pow(phi / (phiCutOff), 10) * Mathf.Atan(controls.camVSpeed/ controls.cameraBgR);
+            
+        }
+        else {
+            phi += Mathf.Atan(controls.camVSpeed / controls.cameraBgR);
         }
 
-        return r;
+        if (Mathf.Abs(phi) > phiCutOff) phi = phi / Mathf.Abs(phi) * phiCutOff;
+        //phi += Mathf.Atan(controls.camVSpeed / controls.cameraBgR);
+
+        radius = (controls.cameraSmllR + radius * (controls.cameraSmooth - 1)) / controls.cameraSmooth;
+
+        direction = new Vector3(Mathf.Sin(phi) * Mathf.Sin(theta), Mathf.Cos(phi), Mathf.Sin(phi) * Mathf.Cos(theta));
+
+        position = new Vector3(Mathf.Sin(phi) * Mathf.Sin(theta), Mathf.Cos(phi),Mathf.Sin(phi) * Mathf.Cos(theta)) *radius + controls.cameraOffset;
+        cameraObject.transform.eulerAngles = new Vector3(phi, theta, 0)*Mathf.Rad2Deg;
+        cameraObject.transform.localPosition = position;
+
+        Debug.DrawRay(cameraObject.transform.position, direction*4, new Color(0, 0, 1));
+
     }
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,46 +4,61 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Misc....")]
     public Vector3 HoVeInput = new Vector3(0,0,0);
-    private Vector3 tempHV;
     public LayerMask collisionMask = 0;
-
+    [Space(0)]
     /// <summary>
     /// WORLD VARS
     /// </summary>
-    public float grav = 0.75f;
-    public float drag = 20;
+    [Header("World controls")]
+    public float grav = 0.75f;          //gravity
+    [Range(0,50)]
+    public float drag = 20;             //doesnt effect verticle drag
+    [Space(0)]
 
     /// <summary>
     /// MOVE VARS
     /// </summary>
-    public float baseSpeed = 1;
-    public float sprintSpeed = 1.5f;
-    public float jumpSpeed = 2;
-
-    public bool isJump = false;
-    public bool reset = false;
-    public bool isSprint = false;
+    [Header("Movement Controls")]
+    [Range(0,10)]
+    public float baseSpeed = 1;         //what speed will return to after sprinting
+    [Range(1,5)]
+    public float sprintSpeed = 1.5f;    //what the new speed will become
+    [Range(0,10)]
+    public float jumpSpeed = 2;         //jump height
+    [Space(0)]
 
     /// <summary>
     /// CAMERA VARS
     /// </summary>
-    public bool mouseLocked = true;
-    public float sensitivity = 1;
-    public float cameraXRot = 30;
-    public float cameraSmooth = 2;
-    public float baseRadius = 6;
-    public float baseHeight = 1;
-    public float theta = 0;
-    public float h = 0;
-    float temp;
-    public float heightLimit = 3;
-    public float cameraSize = 0.5f;
+
+    [Header("Camera Controls")]
+    public CameraController camera;
+    public Vector3 cameraOffset = new Vector3(0,0,0);
+    [Range(0.001f,1)]
+    public float cameraBgR = 10;       //this is the bigger radius that we move the "curso" on
+    [Range(0.1f, 2)]
+    public float cameraSmllR = 1;
+    [Range(1,30)]
+    public float cameraSmooth = 2;      //bigger is smoother
+     
+    public float camHSpeed = 0;
+    public float camVSpeed = 0;
+    private bool mouseLocked = true;
+    [Space(0)]
+
+    [Header("Conditional statements")]
+    public bool isJump = false;
+    public bool reset = false;          //respawn button
+    public bool isSprint = false;
+    
 
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        camera = GetComponent<CameraController>();
     }
 
 
@@ -51,56 +66,44 @@ public class PlayerController : MonoBehaviour
     public void FixedUpdate()
     {
         
-        if      (Input.GetKey(KeyCode.D))   HoVeInput[0] = 1;
-        else if (Input.GetKey(KeyCode.A))   HoVeInput[0] = -1;
-        else                                HoVeInput[0] = 0;
+        if      (Input.GetKey(KeyCode.D))       HoVeInput[0] = 1;
+        else if (Input.GetKey(KeyCode.A))       HoVeInput[0] = -1;
+        else                                    HoVeInput[0] = 0;
 
-        if      (Input.GetKey(KeyCode.W))   HoVeInput[2] = 1;
-        else if (Input.GetKey(KeyCode.S))   HoVeInput[2] = -1;
-        else                                HoVeInput[2] = 0;
+        if      (Input.GetKey(KeyCode.W))       HoVeInput[2] = 1;
+        else if (Input.GetKey(KeyCode.S))       HoVeInput[2] = -1;
+        else                                    HoVeInput[2] = 0;
 
-        if (Input.GetKey(KeyCode.LeftShift)) isSprint = true;
-        else                                 isSprint = false;
+        if (Input.GetKey(KeyCode.LeftShift))    isSprint = true;
+        else                                    isSprint = false;
 
-        if (Input.GetKey(KeyCode.Space))    isJump = true;
-        else                                isJump = false;
+        if (Input.GetKey(KeyCode.Space))        isJump = true;
+        else                                    isJump = false;
         
-        if (Input.GetKey(KeyCode.R))        reset = true;
-        else                                reset = false;
+        if (Input.GetKey(KeyCode.R))            reset = true;
+        else                                    reset = false;
 
-        if (Input.GetKey(KeyCode.Escape)) mouseLocked = false;
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            if (!mouseLocked)                   mouseLocked = true;
+            else                                mouseLocked = false;
+        } 
 
         if (mouseLocked) {
             //Debug.Log("H: " + Input.GetAxis("Mouse X") + "\tV: " + Input.GetAxis("Mouse Y"));
-            theta += Input.GetAxis("Mouse X") * sensitivity*Time.deltaTime;
-            if (Mathf.Abs(theta) > 2 * Mathf.PI) theta -= 2 * Mathf.PI * theta / Mathf.Abs(theta);
-
-            temp = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime*0.5F;
-            if (h * temp > 0)
-            {
-                if (h > 0)
-                {
-                    h += (Mathf.Cos((h) * Mathf.PI / baseHeight) + 1) * temp;
-                    
-                }
-                else {
-                    h += (Mathf.Cos((h) * Mathf.PI / heightLimit) + 1) * temp;
-                }
-                
-            }
-            else h += temp;
-            if (h < -heightLimit) h = -heightLimit;
-            if (h > baseHeight) h = baseHeight;
-
-
+            camHSpeed = Input.GetAxis("Mouse X") * Time.deltaTime;
+            camVSpeed = -Input.GetAxis("Mouse Y") * Time.deltaTime;
         }
         else {
             Cursor.lockState = CursorLockMode.None;
+            camHSpeed = camVSpeed = 0;
         }
-        tempHV = Vector3.Normalize(HoVeInput);
 
-        HoVeInput[0] =  tempHV[0]*Mathf.Cos(-theta) - tempHV[2]* Mathf.Sin(-theta);
-        HoVeInput[2] =  tempHV[0]*Mathf.Sin(-theta) + tempHV[2]* Mathf.Cos(-theta);
+        //I know I could reduce this down to one line, but it would look very ugly, because I would have to 
+        HoVeInput = Vector3.Normalize(new Vector3(  HoVeInput[0] * Mathf.Cos(-camera.theta) - HoVeInput[2] * Mathf.Sin(-camera.theta), 
+                                                    0, 
+                                                    HoVeInput[0] * Mathf.Sin(-camera.theta) + HoVeInput[2] * Mathf.Cos(-camera.theta)));
+
+        
     }
 
 }
