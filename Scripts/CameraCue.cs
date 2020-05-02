5ff6ce1af3597da60870ace4517ae7af5ffba232
Hitch_Hawk <47764116+HitchyHawk@users.noreply.github.com>
2020-05-01 18:17:58 -0700

    Needed to change directory to include new editor folder which adds handles to the camera cue. Also included now is a 3 point camera rail system with curves. Still more functionality that I want to add. But thats going to happen later.

---
+++ Scripts/CameraCue.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCue : MonoBehaviour
+{
+    [HideInInspector] public float theta;
+    [HideInInspector] public float phi;
+    [HideInInspector] public Vector3 position;
+    public Vector3[] points = new Vector3[3] {
+        new Vector3(0f,1f,0f),
+        new Vector3(0f,2f,0f),
+        new Vector3(0f,3f,0f)
+    };
+    [Range(0             ,Mathf.PI*2   )]public float[] targetTheta  = new float[3] {0f,0f,0f};
+    [Range(-Mathf.PI*0.5f,Mathf.PI*0.5f)]public float[] targetPhi    = new float[3] {0f,0f,0f};
+    
+    /// <summary>
+    /// Conditional Statements for custom shtuff
+    /// </summary>
+    [Header("Smoothness")]
+    [Range(50, 300)] public float CueSmoothness = 100;
+    [Range(0.1f, 20)] public float timeTran = 10;
+    [Range(0.1f, 20)] public float timeMiddle = 5;
+
+    public bool allowedToMove = true;
+    public bool lockMovement = false;
+    public bool removeCueAfter = false;
+
+    [HideInInspector]public float currentTime = 0;
+    [HideInInspector]public bool activate;
+
+    private Vector3 direction;
+
+    public void Reset()
+    {
+        points = new Vector3[3] {
+            new Vector3(0f,1f,0f),
+            new Vector3(0f,2f,0f),
+            new Vector3(0f,3f,0f)
+        };
+
+        targetTheta = new float[3] { 0f, 0f, 0f };
+        targetPhi = new float[3] { 0f, 0f, 0f };
+    }
+    private void Start() {
+        position = points[0];
+    }
+
+    public Vector3 GetPoint(float t)
+    {
+        return transform.TransformPoint(GetPoint(points[0], points[1], points[2], t));
+    }
+    public Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t) {
+        t = Mathf.Clamp01(t);
+        float oneMinusT = 1f - t;
+        return oneMinusT * oneMinusT * p0 + 2f * oneMinusT * t * p1 + t * t * p2;
+    }
+
+    private void Update()
+    {
+        if (activate && currentTime < timeTran)
+        {
+            currentTime += Time.deltaTime;
+            position    = GetPoint(currentTime/timeTran);
+
+            for (int i = 0; i <= 2; i++) {
+                if (targetTheta[i] < 0         ) targetTheta[i] += Mathf.PI * 2;
+                if (targetTheta[i] > Mathf.PI*2) targetTheta[i] -= Mathf.PI * 2;
+            }
+
+            if (currentTime < timeMiddle)
+            {
+                if      (Mathf.Abs(targetTheta[1] - targetTheta[0]) > Mathf.Abs(targetTheta[1] - (targetTheta[0] + Mathf.PI * 2))) targetTheta[1] -= Mathf.PI * 2;
+                else if (Mathf.Abs(targetTheta[1] - targetTheta[0]) > Mathf.Abs(targetTheta[1] - (targetTheta[0] - Mathf.PI * 2))) targetTheta[1] += Mathf.PI * 2;
+
+                theta   = (targetTheta[1] - targetTheta[0]) * currentTime / timeMiddle + targetTheta[0];
+                phi     = (targetPhi[1]   - targetPhi[0])   * currentTime / timeMiddle + targetPhi[0];
+            }
+            else {
+                if      (Mathf.Abs(targetTheta[2] - targetTheta[1]) > Mathf.Abs(targetTheta[2] - (targetTheta[1] + Mathf.PI * 2))) targetTheta[2] -= Mathf.PI * 2;
+                else if (Mathf.Abs(targetTheta[2] - targetTheta[1]) > Mathf.Abs(targetTheta[2] - (targetTheta[1] - Mathf.PI * 2))) targetTheta[2] += Mathf.PI * 2;
+
+                theta   = (targetTheta[2] - targetTheta[1]) * (currentTime-timeMiddle) / (timeTran - timeMiddle) + targetTheta[1];
+                phi     = (targetPhi[2]   - targetPhi[1])   * (currentTime-timeMiddle) / (timeTran - timeMiddle) + targetPhi[1];
+            }
+            
+        }
+        else {
+            activate = false;
+            if (currentTime > timeTran) {
+                allowedToMove = true;
+                if (removeCueAfter) Destroy(gameObject);
+            }
+        }
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i <= 2; i++) {
+            direction = new Vector3(Mathf.Sin(targetPhi[i] + Mathf.PI * 1.5f) * Mathf.Sin(targetTheta[i] + Mathf.PI),
+                                    Mathf.Cos(targetPhi[i] + Mathf.PI * 1.5f),
+                                    Mathf.Sin(targetPhi[i] + Mathf.PI * 1.5f) * Mathf.Cos(targetTheta[i] + Mathf.PI));
+            Gizmos.color = new Color(1 - i *0.5f, 1 - i*0.5f,1 - i*0.5f);
+            Gizmos.DrawSphere(points[i] + transform.position, 0.5f);
+            Gizmos.DrawRay(points[i] + transform.position, direction * 4);
+        }
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,215 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [HideInInspector] public Vector3 HoVeInput          = new Vector3(0, 0, 0);
+                      public LayerMask collisionMask    = 0;
+    /// <summary>
+    /// WORLD VARS
+    /// </summary>
+    [Header("World controls")]
+    [Range(0,  2)]  public float grav = 0.75f;  //gravity
+    [Range(0,100)]  public float drag = 20;     //only effects drag on the horizontal, not verticle
+
+    [Space(0)]
+
+    /// <summary>
+    /// MOVE VARS
+    /// </summary>
+    [Header("Movement Controls")]
+    [Range(0, 4)] public float baseSpeed    = 1;        //what speed will return to after sprinting
+    [Range(1, 5)] public float sprintSpeed  = 1.5f;     //what the basespeed is multiplied by
+    [Range(0,50)] public float jumpSpeed    = 2;        //jump speed
+    [Range(0, 3)] public float maxSpeed     = 5;        //The maximum speed the player can have
+    [Space(0)]
+    [HideInInspector] public bool isJump = false;       //did the jump button get pushed
+    [HideInInspector] public bool reset = false;        //did the respawn button get pushed
+    [HideInInspector] public bool isSprint = false;     //did the sprint button get pushed
+
+    /// <summary>
+    /// CAMERA VARS
+    /// </summary>
+
+    [Header("Camera Controls")]
+    public GameObject camera;                                       //The camera we control, we tell it where to go and what rotation it should have
+    public CameraPlayerCue cameraPlayerCue;                         //the camera cue that we defualt to. tells camera where to go
+    public Vector3 cameraOffset = new Vector3(0,0,0);               //the offset of rotation, still need to work on this so it rotates too.
+
+    [Range(0   , 20)] public float cameraSensitivity    = 10;       //this is the bigger radius that we move the "curso" on
+    [Range(0.1f, 10)] public float cameraRadius         = 1;        //How far away the camera is from the offset it rotates around.
+    [Range(1   ,100)] public float cameraSmoothBase     = 30;       //bigger is smoother
+                      private float cameraSmooth;
+
+    [HideInInspector] public Vector2 camSpeed       = Vector2.zero; //For the CameraPlayer Cue
+                      private bool mouseLocked      = true;         //Is the mouse locked in the center of the screeen?
+    [HideInInspector] public Vector3 targetPos      = Vector3.zero; //The target pos that the camera pos is trying to get to
+    [HideInInspector] public Vector3 targetAngleTP  = Vector3.zero; //The target angle that theta is trying to get to
+
+    [HideInInspector] public float theta = 0;                       //current theta rotation of the camera. goes from 0 to 2*pi
+    [HideInInspector] public float phi = 0;                         //current phi rotation of the camera. goes from -pi to pi. depends on the cameraCue phi cutoff
+    
+                      private Vector3 preHoVeInput;                 //used to the previous movement option, not dependent on camera rotation, for transition
+                      private float   preTheta;                     //the last theta before changing camera cue,
+                      public bool     inTransition    = false;      //did you enter or exit a camera cue?
+                      private bool    inCue           = false;      //are you in a cameraCue zone
+    [HideInInspector] public bool     allowedToMove   = true;       //allowed to move, part of a movement option in camera cue, USED IN KEYS
+                      private CameraCue cue;                        //the cue object.
+
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        cameraPlayerCue = GetComponent<CameraPlayerCue>();
+    }
+
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+        //What keys got pushed?
+        {
+            if (cue == null) {
+                allowedToMove   = true;
+                inCue           = false;
+                inTransition    = false;
+            }
+
+            if (allowedToMove)
+            {
+                if (Input.GetKey(KeyCode.D))        HoVeInput[0] = 1;
+                else if (Input.GetKey(KeyCode.A))   HoVeInput[0] = -1;
+                else                                HoVeInput[0] = 0;
+
+                if (Input.GetKey(KeyCode.W))        HoVeInput[2] = 1;
+                else if (Input.GetKey(KeyCode.S))   HoVeInput[2] = -1;
+                else                                HoVeInput[2] = 0;
+
+                if (Input.GetKey(KeyCode.Space))    isJump = true;
+                else                                isJump = false;
+            }
+            else {
+                HoVeInput *= 0;
+            }
+
+            if      (Input.GetKey(KeyCode.LeftShift))   isSprint = true;
+            else                                        isSprint = false;
+
+            if      (Input.GetKey(KeyCode.R))           reset = true;
+            else                                        reset = false;
+
+            if      (Input.GetKeyDown(KeyCode.P))
+            {
+                if (!mouseLocked)   mouseLocked = true;
+                else                mouseLocked = false;
+            }
+        }
+
+        //camera rotation stuff
+        {
+            if (mouseLocked) {
+                camSpeed.x = Input.GetAxis("Mouse X") * Time.deltaTime;
+                camSpeed.y = -Input.GetAxis("Mouse Y") * Time.deltaTime;
+            } 
+            else {
+                Cursor.lockState = CursorLockMode.None;
+                camSpeed = Vector2.zero;
+            }
+
+            //to minimize floating point errors
+            if (theta > 2 * Mathf.PI && cameraPlayerCue.theta > 2 * Mathf.PI) {
+                theta                   -= 2 * Mathf.PI;
+                cameraPlayerCue.theta   -= 2 * Mathf.PI;
+            }
+            else if (theta < 0 && cameraPlayerCue.theta < 0) {
+                theta                   += 2 * Mathf.PI;
+                cameraPlayerCue.theta   += 2 * Mathf.PI;
+            }
+
+            //dealing with cues
+            if (inCue)
+            {
+                targetPos = cue.position;
+                targetAngleTP.y = cameraPlayerCue.theta = cue.theta;
+                targetAngleTP.x = cameraPlayerCue.phi   = -cue.phi;
+                cameraSmooth = cue.CueSmoothness;
+                if (Mathf.Abs(targetAngleTP.y - theta) > Mathf.Abs(targetAngleTP.y - (theta + Mathf.PI * 2)))
+                {
+                    //Debug.Log("TEST");
+                    targetAngleTP.y -= Mathf.PI * 2;
+                    cameraPlayerCue.theta -= Mathf.PI * 2; //update both to activate the minimize floating point error
+                }
+                else if (Mathf.Abs(targetAngleTP.y - theta) > Mathf.Abs(targetAngleTP.y - (theta - Mathf.PI * 2))) {
+                    //Debug.Log("TEST2");
+                    targetAngleTP.y += Mathf.PI * 2;
+                    cameraPlayerCue.theta += Mathf.PI * 2; //update both to activate the minimize floating point error
+                }
+            }
+            else {
+                targetPos       = cameraPlayerCue.position + transform.position;
+                targetAngleTP.y = cameraPlayerCue.theta;
+                targetAngleTP.x = cameraPlayerCue.phi;
+                cameraSmooth    = (cameraSmoothBase - cameraSmooth) *0.01f + cameraSmooth;
+            }
+
+            //the smoothing bit that finallizes it.
+            camera.transform.position   = (targetPos - camera.transform.position)   / cameraSmooth + camera.transform.position;
+            theta                       = (targetAngleTP.y - theta)                 / cameraSmooth + theta;
+            phi                         = (targetAngleTP.x - phi)                   / cameraSmooth + phi;
+
+            camera.transform.eulerAngles = new Vector3(phi, theta, 0) * Mathf.Rad2Deg;
+
+            //applying it to movement and camera
+            if (allowedToMove) {
+                if (inTransition) {
+                    //for locked movement when switching cues.
+                    if (preHoVeInput == HoVeInput) {
+                        HoVeInput = Vector3.Normalize(new Vector3(HoVeInput[0] * Mathf.Cos(-preTheta) - HoVeInput[2] * Mathf.Sin(-preTheta),
+                                                                  0,
+                                                                  HoVeInput[0] * Mathf.Sin(-preTheta) + HoVeInput[2] * Mathf.Cos(-preTheta)));
+                    } else inTransition = false;
+                } else {
+                    preTheta = theta;
+                    preHoVeInput = HoVeInput;
+                    HoVeInput = Vector3.Normalize(new Vector3(HoVeInput[0] * Mathf.Cos(-theta) - HoVeInput[2] * Mathf.Sin(-theta),
+                                                              0,
+                                                              HoVeInput[0] * Mathf.Sin(-theta) + HoVeInput[2] * Mathf.Cos(-theta)));
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        inCue = true;
+        cue = other.GetComponent<CameraCue>();
+
+        //activates the transition movement locking
+        if (cue.lockMovement)   inTransition = true;
+        else                    inTransition = false;
+        
+        //activates the camera movement
+        cue.activate = true;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        allowedToMove = cue.allowedToMove;
+    }
+    void OnTriggerExit(Collider other)
+    { 
+        inCue = false;
+        cue = null;
+
+        //activates the transition movement locking
+        if (cue.lockMovement)   inTransition = true;
+        else                    inTransition = false;
+
+        //reset the timeings
+        cue.currentTime = 0;
+    }
+
+
+}
